fa6b1d2ebafb34e2111907e00e2b43a2ba5497ee
lab596 <86579136+lab596@users.noreply.github.com>
2022-06-06 14:32:00 -0700

    Create countingfunction.cpp

---
+++ countingfunction.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+//Note: functions all must be written before the main method!
+
+void printFunction(){
+  int i =1;
+  while(i<=20){
+    cout<<i<<" ";
+    i++;
+  }
+}
+
+int main(){
+  printFunction();
+  return 0;
+}
