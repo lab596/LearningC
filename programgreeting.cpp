b7705d61dc922bdb48ce703fff7a3ff4499f6a66
lab596 <86579136+lab596@users.noreply.github.com>
2022-06-09 23:23:30 -0700

    Create programgreeting.cpp

---
+++ programgreeting.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //Program Greeting
+  cout<<string(33,'*')<<endl;
+  cout<<"* Programming Assignment1       *\n";
+  cout<<"* CISP 360 Cali Lake College  *\n";
+  cout<<"*       Author: Me!             *\n";
+  cout<<"* Due Date: Sunday, January 1   *\n";
+  cout<<string(33,'*')<<endl;
+
+
+  return 0;
+}
